0aca417aff80c8efd8ef37e34ea7f0658641a3c8
ArthurMelzi <arthur_santos159@estudante.sesisenai.org.br>
2026-05-21 12:21:10 -0300

    Pacote

---
+++ exercicio_2.js
@@ -0,0 +1,10 @@
+// Escreva um código que verifique se um número é par ou ímpar e exiba o resultado no console.
+// para se um numero é par você precisa calcular o modulo, em js é representado com "%"
+
+let num = prompt(Number("Digite um número: "))
+
+if (num % 2 ===  0) {
+    console.log("O número " + num + " é par")
+} else {
+    console.log("O número " + num + " é ímpar")
+}
