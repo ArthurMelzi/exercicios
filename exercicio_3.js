66ee94899b3dc6cd049529d66cf3138b68c12d5b
Arthur Melzi <arthur_santos159@estudante.sesisenai.org.br>
2026-05-22 08:32:44 -0300

    Fix writefile to writeFile in exercicio_3.js

--- exercicio_3.js
+++ exercicio_3.js
@@ -1,8 +1,9 @@
 const fs = require("fs")
 
-fs.writefile("mensagem.txt", "Node.js é poderoso!", "utf-8", (err) => {
+fs.writeFile("mensagem.txt", "Node.js é poderoso!", "utf-8", (err) => {
     if (err) {
         console.log("Erro: ", err)
     }
     console.log("Arquivo criado com sucesso!")
 });
+// nn deu pra atualizar pelo vs code, o commit nn foi
